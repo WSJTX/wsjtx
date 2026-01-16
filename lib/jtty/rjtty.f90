0816af8ab5f9b323a402555d6eae4454ab3f938a
k1jt <joe@princeton.edu>
2026-01-16 14:54:59 -0500

    If JTTY decode is successful, step ahead by NFRAME samples rather than NFRAME/4.

--- lib/jtty/rjtty.f90
+++ lib/jtty/rjtty.f90
@@ -13,7 +13,8 @@ program rjtty
 ! so maximum txt = 16*53*0.032 = 27.136 s.
 
    parameter (NMAX=30*12000)                 !Max length of data
-   parameter (NSPB=2048)                     !Samples per buffer
+   parameter (NFRAME=53*384)
+   parameter (NCHUNK=NFRAME + NFRAME/4)
    type(hdr) h
    character*80 fname
    character*80 umsg
@@ -21,7 +22,6 @@ program rjtty
    integer*8 count0, count1, clkfreq
    integer*2 iwave(NMAX)
    logical synced,success
-   data synced/.false./
 
    f0=1500.0
    ftol=50.0
@@ -49,25 +49,21 @@ program rjtty
       nwave=min(h%ndata/2,NMAX)
       read(10) iwave(1:nwave)
       close(10)
-      iwave(nwave+1:) = 0
-
-      synced=.false.
-      nchunk=53*384+53*384/4
-      nframe=53*384
+      if(nwave.lt.NMAX) iwave(nwave+1:NMAX) = 0
       kchar=0
       istart=1
 
 ! Process data on the fly, one buffer at a time:
-      do while (istart+nchunk-1 .le. nwave)
+      do while (istart+NCHUNK-1 .le. nwave)
          synced=.false.                      ! sync on evey call for now
          success=.false.
  
          call system_clock(count0,clkfreq)
-         call jtty_decode(iwave(istart),nchunk,f0,ftol,smin,synced,xdt,  &
+         call jtty_decode(iwave(istart),NCHUNK,f0,ftol,smin,synced,xdt,  &
             f1,snr,umsg,success,nharderrors,nsync)
          call system_clock(count1,clkfreq)
 
-         if(synced) then
+         if(success) then
             n = len(trim(umsg))
             do i=1,n
                if(umsg(i:i).eq.'~') umsg(i:i)=' '
@@ -80,11 +76,15 @@ program rjtty
                write(*,*) 'debug ',umsg(1:n)
             endif
             tdecode=float(count1-count0)/clkfreq
-            if(ndebug.gt.0) write(71,3071) istart,xdt,f1,snr,synced,nsync,nharderrors,tdecode, &
-               trim(umsg)
-3071        format(i8,f7.3,f8.1,f6.1,L3,i5,i5,f7.3,2x,a)
+            if(ndebug.gt.0) then
+               write(71,3071) istart,xdt,f1,snr,synced,success,nsync,  &
+                    nharderrors,tdecode,trim(umsg)
+3071           format(i8,f7.3,f8.1,f6.1,2L3,i5,i5,f7.3,2x,a)
+            endif
+            istart=istart+NFRAME
+         else
+            istart=istart+NFRAME/4
          endif
-         istart=istart+nframe/4
       enddo
       write(*,*) ''
    enddo  !ifile
