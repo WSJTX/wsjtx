4afde90d2466b5a92fcd53be704f0320f344d4ef
Joe Taylor <joe@princeton.edu>
2026-02-10 12:40:06 -0500

    Fix the output from rjtty for multi-signal decodes.

--- lib/jtty/rjtty.f90
+++ lib/jtty/rjtty.f90
@@ -19,8 +19,7 @@ program rjtty
    character*8 arg
    integer*8 count0, count1, clkfreq
    integer*2 iwave(NMAX)
-   logical synced,success
-
+   logical synced,success,newsig
     
    f0=1500.0
    ftol=50.0
@@ -50,6 +49,7 @@ program rjtty
 
    nframe = 53*nsps
    nchunk = nframe + nframe/4
+   ndecodes = 0
 
    do ifile=1,nargs-5
       call getarg(ifile+5,fname)
@@ -63,6 +63,11 @@ program rjtty
       istart=1
       synced=.false. 
       nsync=0
+      f1good = -99.
+      xdtgood = -99.
+      missed_syncs = 0
+      newsig = .false.
+
 ! Process data on the fly, one buffer at a time:
       do while (istart+nchunk-1 .le. nwave)
          success=.false.
@@ -71,29 +76,47 @@ program rjtty
          call jtty_decode(iwave(istart),nchunk,nsps,f0,ftol,smin,synced,xdt,  &
             f1,snr,umsg,success,nharderrors,nsync,dmin)
          call system_clock(count1,clkfreq)
+
+         if(synced) then
+            missed_syncs=0
+         else
+            missed_syncs = missed_syncs + 1
+            if(missed_syncs.ge.2) then
+               f1good = -99.
+               xdtgood = -99.
+               missed_syncs = 0
+               newsig = .false.
+            endif
+         endif
+
          if(success) then
+            ndecodes = ndecodes + 1
+            newsig = .false.
+            if(abs(f1-f1good).gt.2.0 .or. abs(xdt-xdtgood).gt.0.004) then
+               newsig = .true.
+               if(ndecodes.gt.1) write(*,'(a)')         !Advance to next display line
+            endif
+            f1good = f1
+            xdtgood = xdt
+
+            if(ndebug.gt.0) then
+               tdecode=float(count1-count0)/clkfreq
+               write(71,3071) istart,xdt,f1,snr,synced,success,newsig,kchar,   &
+                    missed_syncs,nsync,nharderrors,dmin,tdecode,trim(umsg)
+3071           format(i8,f7.3,f7.1,f6.1,3L2,4i4,f7.1,f7.3,2x,a)
+            endif
+
             if(umsg(1:4).eq.'599 ') umsg='~'//trim(umsg)
             n = len(trim(umsg))
             do i=1,n
                if(umsg(i:i).eq.'~') umsg(i:i)=' '
             enddo
-            kchar = kchar + n
-            if(kchar.lt.80) then
-               write(*,'(a)',advance='no') umsg(1:n)
-               if(n.ge.3) then
-                  if(umsg(n-2:n).eq.' CQ') write(*,'(a)',advance='no') ' '
-               endif
-            else
-               kchar=0
-               write(*,'(a)') umsg(1:n)
-!               write(*,*) 'debug ',umsg(1:n)
+            kchar = min(kchar + n, 80)
+            write(*,'(a)',advance='no') umsg(1:n)
+            if(n.ge.3) then
+               if(umsg(n-2:n).eq.' CQ') write(*,'(a)',advance='no') ' '
             endif
-            tdecode=float(count1-count0)/clkfreq
-            if(ndebug.gt.0) then
-               write(71,3071) istart,xdt,f1,snr,synced,success,nsync,  &
-                    nharderrors,dmin,tdecode,trim(umsg)
-3071           format(i8,f7.3,f8.1,f6.1,2L3,i5,i5,f9.1,f7.3,2x,a)
-            endif
+
             istart=istart+nframe
             if(nsync.lt.12) synced=.false.
          else
