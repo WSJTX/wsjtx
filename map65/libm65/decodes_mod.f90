22849b50cf507128d5b4729fb596025dea31f9fc
Roger Rehr <73w3sz@gmail.com>
2026-02-15 01:08:40 -0500

    Whitespace change to decodes_mod.f90

--- map65/libm65/decodes_mod.f90
+++ map65/libm65/decodes_mod.f90
@@ -1,23 +1,23 @@
 module decodes_mod
 
-  	implicit none
+    implicit none
 
-	integer, parameter :: NLD = 32768
-	! from decodes
-	integer :: ndecodes
-	
-	!from early 
-	integer :: nhsym1, nhsym2
+    integer, parameter :: NLD = 32768
+    ! from decodes
+    integer :: ndecodes
+    
+    !from early 
+    integer :: nhsym1, nhsym2
 
-	logical, allocatable :: ldecoded(:)
-	
-	!from c3com
-	integer :: mcall3a
+    logical, allocatable :: ldecoded(:)
+    
+    !from c3com
+    integer :: mcall3a
 
-	contains
+    contains
 
-	  subroutine decodes_init()
-	    if (.not. allocated(ldecoded)) allocate(ldecoded(NLD))
-	  end subroutine decodes_init
+      subroutine decodes_init()
+        if (.not. allocated(ldecoded)) allocate(ldecoded(NLD))
+      end subroutine decodes_init
 
 end module decodes_mod
