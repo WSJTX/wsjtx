c7df4acedf9413f5c18fcd8a681ee49b1ae78573
Steve Franke <s.j.franke@icloud.com>
2026-02-19 16:49:15 -0600

    Fix a bug in msk144sync.f90. Thanks to WA6BEV!

--- lib/msk144sync.f90
+++ lib/msk144sync.f90
@@ -67,7 +67,7 @@ subroutine msk144sync(cdat,nframes,ntol,delf,navmask,npeaks,fc,fest,   &
   if2=if1+nstep-1
   if(id.eq.nthreads) if2=nint(ntol/delf)
   call msk144_freq_search(cdat,fc,if1,if2,delf,nframes,navmask,cb,    &
-       cdat2(1,id),xm(id),bf(id),cs(1,id),xccs(1,id))
+       cdat2(1,id),xm(id),bf(id),cs(1,id),xccs(:,id))
 !$OMP END PARALLEL
 
   xmax=xm(1)
