f486d086c9a7680abc3fc842ec6bd5d51db3b054
Joe Taylor <jtaylor@princeton.edu>
2026-02-23 12:16:07 -0500

    Most cleanups finished for JTTY decodes and their display in WSJT-X.

--- lib/jtty/rjtty_sub.f90
+++ lib/jtty/rjtty_sub.f90
@@ -2,12 +2,10 @@ subroutine rjtty_sub(iwave,kz,nsps,f0,ftol)
 
   use jtty_mdec
   integer*2 iwave(kz)
-  character*80 line
   character*80 umsg
-  logical synced,success,newsig
-  data kz0/9999999/,f1good/-99./,xdtgood/-99./,missed_syncs/0/,newsig/.false./
-  save istart,kz0,kchar,line,success,synced,f1good,xdtgood,missed_syncs,  &
-       newsig,ndtol
+  logical synced,success
+  data kz0/9999999/,missed_syncs/0/
+  save istart,kz0,kchar,success,synced,missed_syncs,ndtol
 
   if(nsps.ne.240 .and. nsps.ne.320 .and. nsps.ne.384 .and. nsps.ne.480) return
 
@@ -19,7 +17,6 @@ subroutine rjtty_sub(iwave,kz,nsps,f0,ftol)
      kz0=kz
      istart=1
      kchar=0
-     line=""
      synced=.false.
      ndtol=0
      go to 999
@@ -27,16 +24,15 @@ subroutine rjtty_sub(iwave,kz,nsps,f0,ftol)
   if(kz-istart+1 .lt. nchunk) return      ! wait for enough data
 
   nsync=0
-  dmin=0.0         !Nonzero returned if OSD produced decode. Use to reject false decodes?
+  dmin=0.0    !Nonzero returned if OSD successful. Use to reject false decodes?
   do while (istart+nchunk-1 .le. kz)
      success=.false.
 !     synced=.false.             ! uncomment this to disable use of prior sync 
-
-     ndebug=-1  !### TEMPORARY ??? ###
+     ndebug=-1
      snr=-99.0
      call jtty_mdecode(istart,iwave(istart),nchunk,nsps,ndebug,f0,ftol, &
               smin,synced,xdt,f1,snr,umsg,success,nharderrors,nsync,dmin)
-
+     
      if(synced) then
         missed_syncs = 0
      else
@@ -45,39 +41,11 @@ subroutine rjtty_sub(iwave,kz,nsps,f0,ftol)
            f1good = -99.
            xdtgood = -99.
 !           missed_syncs = 0
-           newsig = .false.
         endif
      endif
 
      if(success) then
-
         ndecodes = ndecodes + 1
-        newsig = .false.
-        if(abs(f1-f1good).gt.2.0 .or. abs(xdt-xdtgood).gt.0.004) then
-           newsig = .true.
-           kchar = 0
-        endif
-        f1good = f1
-        xdtgood = xdt
-
-!        tdecode=0
-!        write(72,3072) istart,xdt,f1,snr,synced,success,newsig,kchar,   &
-!             missed_syncs,nsync,nharderrors,dmin,tdecode,trim(umsg)
-!3072    format(i8,f7.3,f7.1,f6.1,3L2,4i4,f7.1,f7.3,2x,a)
-
-        if(umsg(1:4).eq.'599 ') umsg='~'//trim(umsg)
-        n = len(trim(umsg))
-        if(n.gt.79) n=79                 ! truncate at 80 chars
-        do i=1,n
-           if(umsg(i:i).eq.'~') umsg(i:i)=' '
-        enddo
-        if(newsig) then
-           umsg=char(10)//trim(umsg(1:79))   ! Insert LF
-           n=n+1
-        endif
-        line(kchar+1:kchar+n)=umsg(1:n)
-        line(kchar+n+1:kchar+n+1)=char(0)
-        kchar = min(kchar + n, 80)
         istart=istart+nframe
 ! Use this sync for next frame only if was strong strong
         if(nsync.lt.12) synced=.false.
@@ -85,8 +53,6 @@ subroutine rjtty_sub(iwave,kz,nsps,f0,ftol)
         istart=istart+nframe/4
         synced=.false.
      endif
-     i0=index(line,' CQCQ ')
-     if(i0.ge.6) line=line(1:i0+2)//' '//trim(line(i0+3:))
   enddo
 
 999 return
