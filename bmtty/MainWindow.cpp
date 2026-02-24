e6e5af73b58b40fcda563bc115c9ad07eeac36e6
Brian Moran <brianmoran@gmail.com>
2026-02-23 20:19:49 -0800

    small utility to more easily test mmtty interface

--- bmtty/MainWindow.cpp
+++ bmtty/MainWindow.cpp
@@ -35,7 +35,7 @@ MainWindow::MainWindow(const CommandLineOptions &options, QWidget *parent)
     
     m_textEdit = new QTextEdit(this);
     m_textEdit->setReadOnly(true);
-    
+
     QString info = "<b>Boolean Flags:</b><br>";
     for (auto it = m_options.flags.begin(); it != m_options.flags.end(); ++it) {
         info += QString("-%1: %2<br>").arg(it.key()).arg(it.value() ? "ON" : "OFF");
@@ -48,7 +48,7 @@ MainWindow::MainWindow(const CommandLineOptions &options, QWidget *parent)
     
     m_textEdit->setHtml(info);
     layout->addWidget(m_textEdit);
-    
+
     setCentralWidget(centralWidget);
     resize(500, 400);
 
