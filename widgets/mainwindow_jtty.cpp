359249abf03a3bd10421ca2ba5d7c30e0c9c6fe4
Joe Taylor <jtaylor@princeton.edu>
2026-03-09 11:57:20 -0400

    Satisfy a gcc warning message.

--- widgets/mainwindow_jtty.cpp
+++ widgets/mainwindow_jtty.cpp
@@ -162,7 +162,7 @@ void MainWindow::on_TxFreqSpinBox_2_valueChanged(int n)
 
 void MainWindow::on_sbFtol_2_valueChanged (int n)
 {
-//    if(n==999) std::cout << "AAA " << n << "\n";
+    if(n==999) std::cout << "AAA " << n << "\n";
 }
 
 QString MainWindow::jtty_msg_expand(QString t)
