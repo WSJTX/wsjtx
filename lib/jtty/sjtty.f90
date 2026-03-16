2e00d5c94994e5f4f95887fdf55256622a0ef96d
Steve Franke <s.j.franke@icloud.com>
2026-03-16 13:15:31 -0500

    Calling sjtty with just a message will print the number of frames and Tx time.

--- lib/jtty/sjtty.f90
+++ lib/jtty/sjtty.f90
@@ -35,9 +35,25 @@ program sjtty
   data graymap/0,1,3,2/
 
   nargs=iargc()
-  if(nargs.ne.8) then
-     print*,'Usage:   sjtty    message     f0   DT fdop del nsps nfiles SNR'
-     print*,'Example: sjtty "CQ K1ABC CQ" 1500 0.0  0.5  1   384    10   -10'
+  if(nargs.eq.1) then
+    call getarg(1,umsg)
+    call pack_jtty(umsg,c32,nframes)
+    write(*,'(a,a)') "Message : ",trim(umsg)
+    nsps=384
+    if(nframes.eq.1) then
+       write(*,'(i4,a,f5.2,a)') nframes," frame, transmission length", &
+          53*nframes*nsps/12000.0," seconds"
+    else
+       write(*,'(i4,a,f5.2,a)') nframes," frames, Transmission length", &
+          53*nframes*nsps/12000.0," seconds"
+    endif 
+    go to 999
+  else if(nargs.ne.8) then
+     print*,'Usage:     sjtty       message'
+     print*,'Example:   sjtty    "CQ DX KA1ABC"' 
+     print*,'or'
+     print*,'Usage:     sjtty       message     f0   DT fdop del nsps  nfiles SNR'
+     print*,'Example:   sjtty    "CQ K1ABC CQ" 1500 0.0  0.5  1   384    10   -10'
      print*,'ITU propagation models: set fdop to AW LQ LM LD MQ MM MD HQ HM HD'
      print*,'nsps: 240, 320, 384, or 480'
      go to 999
