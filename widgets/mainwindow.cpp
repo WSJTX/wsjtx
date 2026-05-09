841aaae505803bca6c8334302e22a5f627d12b17
Joe Taylor <jtaylor@princeton.edu>
2026-05-09 10:55:48 -0400

    Correct the -devel expiration date.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -1300,7 +1300,7 @@ void MainWindow::not_GA_warning_message ()
   MessageBox::critical_message (this,
                                 "This is a pre-release version of WSJT-X " + version (false) + " made\n"
                                 "available for testing purposes.  By design it will\n"
-                                "be nonfunctional after April 30, 2026.");
+                                "be nonfunctional after September 30, 2026.");
   auto now = QDateTime::currentDateTimeUtc ();
   if (now >= QDateTime {{2026, 9, 30}, {23, 59, 59, 999}, Qt::UTC}) {
     Q_EMIT finished ();
