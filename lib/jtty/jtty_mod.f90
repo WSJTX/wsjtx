c4227d0d3000b736a03b3ff4eef3f2d0223663e3
David Christle <dchristle@users.noreply.github.com>
2026-05-30 01:36:16 -0700

    refactor(jtty): pack normalized messages with grammar DP

--- lib/jtty/jtty_mod.f90
+++ lib/jtty/jtty_mod.f90
@@ -1,10 +1,6 @@
 module jtty_mod
 
-  ! These variables are accessible from outside via "use pack_jtty"
-
-  parameter (MAX_WORDS=40)              !Max words in message
   parameter (MAX_FRAMES=16)             !Max frames for the encoded message
-  logical verbose
 
 contains
 
@@ -17,162 +13,320 @@ subroutine pack_jtty(message,c32,nframes)
   use packjt77
   character*80 message,msg
   character*32 c32(MAX_FRAMES)
-  
-  character*13 w(MAX_WORDS)      !Individual message words
-  integer nw(MAX_WORDS)          !Sizes of message words
-  logical bw(MAX_WORDS)          !bw(i) is True if w(i) is a standard callsign
-  integer*4 nwords               !Number of words in message
-  integer*4 n32
-  logical btext,eom
-  data nmsg/0/
-  save
 
-  nmsg=nmsg+1
-! Convert message to upper case; collapse multiple blanks; parse into words.
-  call split_jtty(message,w,nw,bw,nwords)
+  integer, parameter :: KIND_TEXT=1, KIND_599=2, KIND_STRUCT=3
+  integer, parameter :: RANK_COMPACT=1, RANK_TEXT=2, INF=999
+  integer dp(81), choice_kind(80), choice_next(80), choice_i2(80)
+  integer choice_n2(80), choice_call_first(80), choice_call_len(80)
+  integer best_rank(80), best_next(80)
+  integer n, ipos, n32, lcall, icall, iend
+  integer i2, n2, n28
+  character*13 c13
+  logical ok
 
-  msg=message
-  ntext=80
-  k1=80
-  btext=.false.
-  eom=.false.
-  do iframe=1,MAX_FRAMES
-     i2=-1
-     n2=-1
-     nz=0
-     c32(iframe)=''
-     if(trim(w(1)).eq.'CQ' .and. bw(2) .and. trim(w(3)).eq.'CQ' &
-          .and. .not.btext) then
-        i2=0
-        n2=0
-        nz=3
-        call pack28(w(2),n28)
-        n32=shiftl(n28,4) + 4*n2 + i2
-        write(c32(iframe),1002) n32
-1002    format(b32.32)
-        eom=.true.
-     else if(bw(1) .and. nwords.eq.1 .and. .not.btext) then
-        i2=0
-        n2=1
-        nz=1
-        call pack28(w(1),n28)
-        n32=shiftl(n28,4) + 4*n2 + i2
-        write(c32(iframe),1002) n32
-        eom=.true.
-     else if(trim(w(1)) .eq. 'TU' .and. bw(2) .and. trim(w(3)).eq.'CQ' &
-           .and. .not.btext) then
-        i2=0
-        n2=2
-        nz=3
-        call pack28(w(2),n28)
-        n32=shiftl(n28,4) + 4*n2 + i2
-        write(c32(iframe),1002) n32
-        eom=.true.
-     else if(bw(1) .and. trim(w(2)).eq.'TU' .and. .not.btext) then
-        i2=0
-        n2=3
-        nz=2
-        call pack28(w(1),n28)
-        n32=shiftl(n28,4) + 4*n2 + i2
-        write(c32(iframe),1002) n32
-        if(nwords.eq.2) then
-           eom=.true.
-        else
-           k1=nw(1) + nw(2) + 2
-           msg=msg(k1:)
-           w(1:nwords-2) = w(3:nwords)
-           bw(1:nwords-2) = bw(3:nwords)
-           nw(1:nwords-2) = nw(3:nwords)
-           nwords = nwords - 2
-        endif
-     else if(bw(1) .and. trim(w(2)).eq.'AGN?' .and. .not.btext) then
-        i2=1
-        n2=0
-        nz=2
-        call pack28(w(1),n28)
-        n32=shiftl(n28,4) + 4*n2 + i2
-        write(c32(iframe),1002) n32
-        eom=.true.
-     else if(bw(1)  .and. .not.btext) then
-        i2=0
-        n2=1
-        nz=nwords
-        call pack28(w(1),n28)
-        n32=shiftl(n28,4) + 4*n2 + i2
-        write(c32(iframe),1002) n32
-        k1=nw(1)+2
-        msg=message(k1:)
-        if(verbose) print*,'aa',k1,trim(msg)
-     else if(trim(w(1)).eq.'TU' .and. trim(w(2)).eq.'NOW' .and. bw(3) &
-           .and. .not.btext) then
-        i2=1
-        n2=1
-        nz=3
-        call pack28(w(3),n28)
-        n32=shiftl(n28,4) + 4*n2 + i2
-        write(c32(iframe),1002) n32
-        k1=len(trim(w(3))) + 9
-        msg=message(k1:)
-     else if(msg(1:4).eq.'599 ') then
-        i2=2
-        btext=.true.
-        nz=nwords
-        i0=index(msg,'599 ') + 4
-        n30=0
-        do i=i0,i0+4
-           n30=64*n30 + jchar(msg(i:i))
-        enddo
-        n32=ishft(n30,2) + i2
-        write(c32(iframe),1002) n32
-        eom=.true.
-     else
-        i2=3                             !Use plain text, 5 characters per frame
-        ntext=len(trim(msg))
-        btext=.true.
-        nz=nwords
-        n30=0
-        do i=1,5
-           n30=64*n30 + jchar(msg(i:i))
-        enddo
-        n32=ishft(n30,2) + i2
-        write(c32(iframe),1002) n32
-        msg=msg(6:)
-        ntext=ntext-5
-     endif
+  call normalize_jtty_message(message,msg,ok)
+  if(.not.ok) then
+     nframes=-1
+     c32=''
+     return
+  endif
+  message=msg
+  n=len_trim(msg)
+  nframes=0
+  c32=''
+  if(n.le.0) return
 
-!### Temporary
-     if(verbose) then
-        if(iframe.eq.1) then
-           print*,'PACK'
-           if(i2.ne.3) write(*,5010) nmsg,iframe,i2,n2,trim(message(1:k1-2))
-5010       format(i2,i6,i6,'.',i1,3x,a)
-           if(i2.eq.3) write(*,5011) nmsg,iframe,i2,trim(msg)
-5011       format(i2,i6,i6'.',4x,a)
-        else
-           if(i2.ne.3) write(*,5012) iframe,i2,n2,trim(msg)
-5012       format(2x,i6,i6,'.',i1,3x,a)
-           if(i2.eq.3) write(*,5013) iframe,i2,trim(msg)
-5013       format(2x,i6,i6'.',4x,a)     
-        endif
-     endif
-!### 
+  dp=INF
+  choice_kind=0
+  choice_next=0
+  choice_i2=0
+  choice_n2=0
+  choice_call_first=0
+  choice_call_len=0
+  best_rank=INF
+  best_next=0
+  dp(n+1)=0
 
-     nframes=iframe
-     if(eom) exit
-     if(ntext.le.0) exit
-     if(nz.ge.MAX_WORDS) exit
-     if(nz.lt.1) exit
-     if(nz.le.MAX_WORDS-1 .and. w(nz+1)(1:1).eq.' ' .and. .not.btext &
-          .and. nwords.eq.1) exit
-     do k=1,MAX_WORDS-nz
-        w(k)=w(k+nz)
-        nw(k)=nw(k+nz)
-        bw(k)=bw(k+nz)
-     enddo
+  do ipos=n,1,-1
+     call try_text()
+     call try_599()
+     call try_structured()
   enddo
 
+  ipos=1
+  do while(ipos.le.n .and. nframes.lt.MAX_FRAMES)
+     if(choice_kind(ipos).le.0) exit
+     nframes=nframes+1
+     if(choice_kind(ipos).eq.KIND_STRUCT) then
+        i2=choice_i2(ipos)
+        n2=choice_n2(ipos)
+        c13='             '
+        icall=choice_call_first(ipos)
+        lcall=choice_call_len(ipos)
+        c13(1:lcall)=msg(icall:icall+lcall-1)
+        call pack28(c13,n28)
+        n32=shiftl(n28,4) + 4*n2 + i2
+     else if(choice_kind(ipos).eq.KIND_599) then
+        call pack_599_frame(ipos,n32)
+     else
+        call pack_text_frame(ipos,n32)
+     endif
+     write(c32(nframes),1002) n32
+1002 format(b32.32)
+     ipos=choice_next(ipos)
+  enddo
+
+  if(ipos.le.n) then
+     nframes=-1
+     c32=''
+  endif
+
   return
+
+contains
+
+  subroutine consider(inext,kind,i2arg,n2arg,icallarg,lcallarg,rank)
+    integer inext,kind,i2arg,n2arg,icallarg,lcallarg,rank
+    integer cand
+
+    if(inext.lt.1 .or. inext.gt.n+1) return
+    if(dp(inext).ge.INF) return
+    cand=1 + dp(inext)
+    if(cand.gt.MAX_FRAMES) return
+    ! Canonical ties prefer compact encodings, then the longest source span.
+    if(cand.lt.dp(ipos) .or. &
+         (cand.eq.dp(ipos) .and. rank.lt.best_rank(ipos)) .or. &
+         (cand.eq.dp(ipos) .and. rank.eq.best_rank(ipos) .and. inext.gt.best_next(ipos))) then
+       dp(ipos)=cand
+       best_rank(ipos)=rank
+       best_next(ipos)=inext
+       choice_kind(ipos)=kind
+       choice_next(ipos)=inext
+       choice_i2(ipos)=i2arg
+       choice_n2(ipos)=n2arg
+       choice_call_first(ipos)=icallarg
+       choice_call_len(ipos)=lcallarg
+    endif
+  end subroutine consider
+
+  subroutine try_text()
+    integer j
+
+    j=min(n,ipos+4)
+    if(jtty_text_ok(ipos,j)) call consider(j+1,KIND_TEXT,3,0,0,0,RANK_TEXT)
+  end subroutine try_text
+
+  subroutine try_599()
+    integer j
+
+    if(.not.at_token_start(ipos)) return
+    if(.not.matches(ipos,'599 ')) return
+    j=min(n,ipos+8)
+    if(jtty_text_ok(ipos+4,j)) call consider(j+1,KIND_599,2,0,0,0,RANK_COMPACT)
+  end subroutine try_599
+
+  subroutine try_structured()
+    ! Enumerates assigned structured forms; i2=1,n2=2/3 remain reserved.
+    if(.not.at_token_start(ipos)) return
+
+    if(matches(ipos,'CQ ')) then
+       icall=ipos+3
+       do lcall=3,6
+          if(valid_call_at(icall,lcall) .and. matches(icall+lcall,' CQ')) then
+             iend=icall+lcall+2
+             call consider_structured(iend,0,0,icall,lcall)
+          endif
+       enddo
+    endif
+
+    if(matches(ipos,'TU ')) then
+       icall=ipos+3
+       do lcall=3,6
+          if(valid_call_at(icall,lcall) .and. matches(icall+lcall,' CQ')) then
+             iend=icall+lcall+2
+             call consider_structured(iend,0,2,icall,lcall)
+          endif
+       enddo
+    endif
+
+    if(matches(ipos,'TU NOW ')) then
+       icall=ipos+7
+       do lcall=3,6
+          if(valid_call_at(icall,lcall)) then
+             iend=icall+lcall-1
+             call consider_structured(iend,1,1,icall,lcall)
+          endif
+       enddo
+    endif
+
+    icall=ipos
+    do lcall=3,6
+       if(valid_call_at(icall,lcall)) then
+          if(matches(icall+lcall,' TU')) then
+             iend=icall+lcall+2
+             call consider_structured(iend,0,3,icall,lcall)
+          endif
+          if(matches(icall+lcall,' AGN?')) then
+             iend=icall+lcall+4
+             call consider_structured(iend,1,0,icall,lcall)
+          endif
+          iend=icall+lcall-1
+          call consider_structured(iend,0,1,icall,lcall)
+       endif
+    enddo
+  end subroutine try_structured
+
+  subroutine consider_structured(iendarg,i2arg,n2arg,icallarg,lcallarg)
+    integer iendarg,i2arg,n2arg,icallarg,lcallarg
+    integer jnext
+
+    if(iendarg.eq.n) then
+       jnext=n+1
+    else if(iendarg.lt.n) then
+       if(msg(iendarg+1:iendarg+1).eq.' ') then
+          jnext=iendarg+2
+       else
+          return
+       endif
+    else
+       return
+    endif
+    call consider(jnext,KIND_STRUCT,i2arg,n2arg,icallarg,lcallarg,RANK_COMPACT)
+  end subroutine consider_structured
+
+  logical function matches(istart,text)
+    integer istart
+    character*(*) text
+    integer ltext
+
+    ltext=len(text)
+    matches=istart.ge.1 .and. istart+ltext-1.le.n
+    if(matches) matches=msg(istart:istart+ltext-1).eq.text
+  end function matches
+
+  logical function at_token_start(istart)
+    integer istart
+
+    if(istart.eq.1) then
+       at_token_start=.true.
+    else
+       at_token_start=msg(istart-1:istart-1).eq.' '
+    endif
+  end function at_token_start
+
+  logical function valid_call_at(istart,ltext)
+    integer istart,ltext
+    character*13 calltoken
+
+    valid_call_at=.false.
+    if(istart.lt.1 .or. ltext.lt.1 .or. istart+ltext-1.gt.n) return
+    calltoken='             '
+    calltoken(1:ltext)=msg(istart:istart+ltext-1)
+    valid_call_at=jtty_standard_call(calltoken)
+  end function valid_call_at
+
+  logical function jtty_text_ok(istart,iendarg)
+    integer istart,iendarg
+    integer i
+
+    jtty_text_ok=.true.
+    do i=istart,iendarg
+       if(jchar(msg(i:i)).lt.0) then
+          jtty_text_ok=.false.
+          return
+       endif
+    enddo
+  end function jtty_text_ok
+
+  subroutine pack_text_frame(istart,n32out)
+    integer istart,n32out
+    integer i, n30
+    character*1 c
+
+    n30=0
+    do i=istart,istart+4
+       c=' '
+       if(i.le.n) c=msg(i:i)
+       n30=64*n30 + jchar(c)
+    enddo
+    n32out=ishft(n30,2) + 3
+  end subroutine pack_text_frame
+
+  subroutine pack_599_frame(istart,n32out)
+    integer istart,n32out
+    integer i, n30
+    character*1 c
+
+    n30=0
+    do i=istart+4,istart+8
+       c=' '
+       if(i.le.n) c=msg(i:i)
+       n30=64*n30 + jchar(c)
+    enddo
+    n32out=ishft(n30,2) + 2
+  end subroutine pack_599_frame
+
 end subroutine pack_jtty
+
+logical function jtty_standard_call(c13)
+
+  use packjt77
+  character*13 c13,c13a
+  character*13 unpacked
+  character*6 bcall_1
+  logical ok1,success
+  integer*4 n28
+
+  jtty_standard_call=.false.
+  c13a=c13
+  call chkcall(c13a,bcall_1,ok1)
+  if(.not.ok1) return
+  if(index(c13a,'/').gt.0) return
+  if(c13a(1:1).eq.'Q') return
+  if(verify(trim(c13a),'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789').ne.0) return
+
+  call pack28(c13a,n28)
+  call unpack28(n28,unpacked,success)
+  if(.not.success) return
+  jtty_standard_call=trim(unpacked).eq.trim(c13a)
+
+end function jtty_standard_call
+
+subroutine normalize_jtty_message(raw,normalized,ok)
+
+  character*80 raw,normalized
+  character*1 c
+  logical ok,last_space
+
+  normalized=''
+  ok=.true.
+  last_space=.true.
+  j=0
+
+  do i=1,len(raw)
+     c=raw(i:i)
+     if(ichar(c).eq.0) c=' '
+     ! The decoder uses '~' as a display marker for space, not a source symbol.
+     if(c.eq.'~') c=' '
+     if(c.ge.'a' .and. c.le.'z') c=char(ichar(c)-32)
+     if(jchar(c).lt.0) then
+        ok=.false.
+        normalized=''
+        return
+     endif
+     if(c.eq.' ') then
+        if(last_space) cycle
+        j=j+1
+        normalized(j:j)=' '
+        last_space=.true.
+     else
+        j=j+1
+        normalized(j:j)=c
+        last_space=.false.
+     endif
+  enddo
+
+end subroutine normalize_jtty_message
 
 subroutine unpack_jtty(c32,nframes,message)
   
@@ -185,7 +339,6 @@ subroutine unpack_jtty(c32,nframes,message)
   character*32 c32(MAX_FRAMES)
 
   character*13 c13
-  integer*4 n32
   logical success
 
   message=''
@@ -197,9 +350,6 @@ subroutine unpack_jtty(c32,nframes,message)
      call unpack28(n28,c13,success)
      n=0
      if(success) n=len(trim(c13))
-     read(c32(iframe),1004) n32
-1004 format(b32.32)
-     n32=shiftl(n28,4) + 4*n2 + i2
      if(i2.eq.2 .or. i2.eq.3) then
         read(c32(iframe),1006) n30
 1006    format(b30.30)
@@ -240,78 +390,10 @@ subroutine unpack_jtty(c32,nframes,message)
         endif
      endif
 
-!### Temporary
-!     if(iframe.eq.1) print*,'UNPACK'
-!     if(i2.lt.3) write(*,5001) iframe,i2,n2,trim(message)
-!5001 format(6x,i2,i6,'.',i1,3x,a)
-!     if(i2.eq.3) write(*,5002) iframe,i2,trim(message)
-!5002 format(6x,i2,i6,5x,a)
-!###
-     
   enddo
 
 end subroutine unpack_jtty
 
-
-subroutine split_jtty(message,w,nw,bw,nwords)
-
-! Convert message to upper case; collapse multiple blanks; parse into words.
-
-! Input:  character*80 message
-! Output: character*13 w(1:nw)     words (of maximum length 13) in message
-!         nw(1:nwords)             length of each parsed word
-!         nwords                   number of words in message
-
-  character*80 message
-  character*13 w(MAX_WORDS)      !Individual message words
-  character*1 c,c0
-  character*6 bcall_1
-  logical bw(MAX_WORDS)
-  logical ok1
-  integer nw(MAX_WORDS)
-    
-  iz=len(trim(message))
-  j=0
-  k=0
-  n=0
-  c0=' '
-  w='             '
-  bw=.false.
-  nw=0
-  do i=1,iz
-     if(ichar(message(i:i)).eq.0) message(i:i)=' '
-     c=message(i:i)                             !Single character
-     if(c.eq.' ' .and. c0.eq.' ') cycle         !Skip leading/repeated blanks
-     if(c.ne.' ' .and. c0.eq.' ') then
-        k=k+1                                   !New word
-        n=0
-     endif
-     j=j+1                                      !Index in message
-     n=n+1                                      !Index in word
-     if(c.ge.'a' .and. c.le.'z') c=char(ichar(c)-32)  !Force upper case
-     message(j:j)=c
-     if(n.le.13) w(k)(n:n)=c                    !Copy character c into word
-     c0=c
-  enddo
-  iz=j                                          !Message length
-  nwords=k                                      !Number of words in message
-  if(nwords.le.0) go to 900
-  do i=1,nwords
-     nw(i)=len(trim(w(i)))
-     call chkcall(w(i),bcall_1,ok1)
-     bw(i)=ok1 .and. (index(w(i),'/').le.0)
-  enddo
-  message(iz+1:)='                                     '
-  if(nwords.lt.3) go to 900
-  call chkcall(w(3),bcall_1,ok1)
-  if(ok1 .and. w(1)(1:3).eq.'CQ ') then
-     w(1)='CQ_'//w(2)(1:10)             !Make "CQ " into "CQ_"
-     w(2:12)=w(3:13)                    !Move all remaining words down by one
-     nwords=nwords-1
-  endif
-
-900 return
-end subroutine split_jtty
 
 character*1 function charj(j)
 
