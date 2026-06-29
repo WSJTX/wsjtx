4b1b940825a2bf75a034b2b0bdbe2c2bf68b071c
David Christle <dchristle@users.noreply.github.com>
2026-06-29 13:50:48 -0700

    fix(packjt77): share split tokenizer in var paths

--- lib/77bit/packjt77.f90
+++ lib/77bit/packjt77.f90
@@ -1927,7 +1927,7 @@ subroutine pack77var(msg0,i3,n3,c77,ntxhash)
   if(i3_hint.eq.0 .and. n3_hint.eq.5) go to 5
 
 ! Convert msg to upper case; collapse multiple blanks; parse into words.
-  call split77var(msg,nwords,nw,w)
+  call split77(msg,nwords,nw,w)
   if(msg(1:3).eq.'CQ ' .or. msg(1:3).eq.'DE ' .or. msg(1:4).eq.'QRZ ') go to 100
 
 ! Check 0.1 (DXpedition mode)
@@ -2654,55 +2654,6 @@ subroutine unpack28var(n28_0,c13,success,nthr)
 
   return
 end subroutine unpack28var
-
-subroutine split77var(msg,nwords,nw,w)
-
-! Convert msg to upper case; collapse multiple blanks; parse into words.
-
-  character*37 msg
-  character*13 w(19)
-  character*1 c,c0
-  character*6 bcall_1
-  logical ok1
-  integer nw(19)
-    
-  iz=len(trim(msg))
-  j=0
-  k=0
-  n=0
-  c0=' '
-  w='             '
-  do i=1,iz
-     if(ichar(msg(i:i)).eq.0) msg(i:i)=' '
-     c=msg(i:i)                                 !Single character
-     if(c.eq.' ' .and. c0.eq.' ') cycle         !Skip leading/repeated blanks
-     if(c.ne.' ' .and. c0.eq.' ') then
-        k=k+1                                   !New word
-        n=0
-     endif
-     j=j+1                                      !Index in msg
-     n=n+1                                      !Index in word
-     if(c.ge.'a' .and. c.le.'z') c=char(ichar(c)-32)  !Force upper case
-     msg(j:j)=c
-     if(n.le.13) w(k)(n:n)=c                    !Copy character c into word
-     c0=c
-  enddo
-  iz=j                                          !Message length
-  nwords=k                                      !Number of words in msg
-  if(nwords.le.0) go to 900
-  nw(k)=len(trim(w(k)))
-  msg(iz+1:)='                                     '
-  if(nwords.lt.3) go to 900
-  call chkcall(w(3),bcall_1,ok1)
-  if(ok1 .and. w(1)(1:3).eq.'CQ ') then
-     w(1)='CQ_'//w(2)(1:10)             !Make "CQ " into "CQ_"
-     w(2:12)=w(3:13)                    !Move all remeining words down by one
-     nwords=nwords-1
-  endif
-  
-900 return
-end subroutine split77var
-
 
 subroutine pack77_01var(nwords,w,i3,n3,c77,ntxhash)
 
