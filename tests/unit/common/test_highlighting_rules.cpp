WSJTX/wsjtx

commit 919f96ea2e9f46dd305dfbd640ada052e28b72cc

Fix a small bug regarding the highlighting of directional calls.
(cherry picked from commit 3f0a85223bd9bb2ddf8ba856621605d82b0055c5)

widgets/mainwindow.cpp
@@ -3222,9 +3222,9 @@ void MainWindow::fastSink(qint64 frames)
             ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         }
     }
@@ -7053,9 +7053,9 @@ void MainWindow::readFromStdout()                             //readFromStdout
               ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
           // highlight directional calls
           if (tw.size () > 2) {
-            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
           }
         }

widgets/mainwindow_AL_for_DG2YCB.cpp
@@ -3224,9 +3224,9 @@ void MainWindow::fastSink(qint64 frames)
             ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         }
     }
@@ -7071,9 +7071,9 @@ void MainWindow::readFromStdout()                             //readFromStdout
               ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
           // highlight directional calls
           if (tw.size () > 2) {
-            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
           }
         }

widgets/mainwindow_i+.cpp
@@ -3222,9 +3222,9 @@ void MainWindow::fastSink(qint64 frames)
             ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         }
     }
@@ -7053,9 +7053,9 @@ void MainWindow::readFromStdout()                             //readFromStdout
               ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
           // highlight directional calls
           if (tw.size () > 2) {
-            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
           }
         }

widgets/mainwindow_ial+.cpp
@@ -3222,9 +3222,9 @@ void MainWindow::fastSink(qint64 frames)
             ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         }
     }
@@ -7053,9 +7053,9 @@ void MainWindow::readFromStdout()                             //readFromStdout
               ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
           // highlight directional calls
           if (tw.size () > 2) {
-            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
           }
         }

widgets/mainwindow_ifox+.cpp
@@ -3230,9 +3230,9 @@ void MainWindow::fastSink(qint64 frames)
             ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         }
     }
@@ -7070,9 +7070,9 @@ void MainWindow::readFromStdout()                             //readFromStdout
               ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
           // highlight directional calls
           if (tw.size () > 2) {
-            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
           }
         }

widgets/mainwindow_iws+.cpp
@@ -3221,9 +3221,9 @@ void MainWindow::fastSink(qint64 frames)
             ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         }
     }
@@ -7052,9 +7052,9 @@ void MainWindow::readFromStdout()                             //readFromStdout
               ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
           // highlight directional calls
           if (tw.size () > 2) {
-            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
           }
         }

widgets/mainwindow_wsjtx.cpp
@@ -3223,9 +3223,9 @@ void MainWindow::fastSink(qint64 frames)
             ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
               ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         }
     }
@@ -7054,9 +7054,9 @@ void MainWindow::readFromStdout()                             //readFromStdout
               ui->decodedTextBrowser->highlight_callsign(deGrid, QColor(0,100,255), QColor(255,255,255), true);
           // highlight directional calls
           if (tw.size () > 2) {
-            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
-            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains(tw[1] + ","))
+            if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ","))
                 ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
           }
         }

commit a4e514f5cc39b5bc7b7c8ab112628ffa23578514

fix(gui): restore directional highlight list matching
Directional entries at the start of a list and entries separated by
documented whitespace stopped matching after exact comma boundaries
were added.

Compare trimmed list fields exactly so DX cannot match K1DX while
first, single, and spaced entries continue to drive highlighting and
Wanted alerts.

Fixes: 919f96ea2 ("Fix a small bug regarding the highlighting of directional calls.")

CMakeLists.txt
@@ -420,6 +420,7 @@ set (wsjtx_CXXSRCS
   widgets/colorhighlighting.cpp
   WSPR/WsprTxScheduler.cpp
   AutoRespondSelectionLatch.cpp
+  HighlightingRules.cpp
   SuperFoxTxPlanner.cpp
   widgets/SpecOpLabel.cpp
   widgets/mainwindow.cpp

Configuration.ui
@@ -3141,7 +3141,7 @@ Right click for insert and delete options.</string>
             <item row="0" column="2">
              <widget class="QCheckBox" name="highlight_orange_check_box">
               <property name="toolTip">
-               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns or grids with orange background color. Callsigns and/or grids must be separated by a comma, and there must be a comma after each callsign.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns, grids, prefixes, or directional CQ calls with orange background color. Separate callsigns, grids, and directional calls with commas; spaces after commas are allowed. Include a trailing comma after each callsign or grid, and end each prefix with a semicolon. A leading comma is not required, and a single directional call needs no comma.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
               </property>
               <property name="text">
                <string>Highlight orange:</string>
@@ -3151,7 +3151,7 @@ Right click for insert and delete options.</string>
             <item row="2" column="2">
              <widget class="QCheckBox" name="highlight_blue_check_box">
               <property name="toolTip">
-               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns or grids with blue background color. Callsigns and/or grids must be separated by a comma, and there must be a comma after each callsign.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns, grids, prefixes, or directional CQ calls with blue background color. Separate callsigns, grids, and directional calls with commas; spaces after commas are allowed. Include a trailing comma after each callsign or grid, and end each prefix with a semicolon. A leading comma is not required, and a single directional call needs no comma.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
               </property>
               <property name="text">
                <string>Highlight blue:</string>
@@ -3161,14 +3161,14 @@ Right click for insert and delete options.</string>
             <item row="1" column="2">
              <widget class="QLineEdit" name="highlight_orange_callsigns">
               <property name="toolTip">
-               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns or grids with orange background color. Callsigns and/or grids must be separated by a comma, and there must be a comma after each callsign.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns, grids, prefixes, or directional CQ calls with orange background color. Separate callsigns, grids, and directional calls with commas; spaces after commas are allowed. Include a trailing comma after each callsign or grid, and end each prefix with a semicolon. A leading comma is not required, and a single directional call needs no comma.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
               </property>
              </widget>
             </item>
             <item row="3" column="2">
              <widget class="QLineEdit" name="highlight_blue_callsigns">
               <property name="toolTip">
-               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns or grids with blue background color. Callsigns and/or grids must be separated by a comma, and there must be a comma after each callsign.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Highlight callsigns, grids, prefixes, or directional CQ calls with blue background color. Separate callsigns, grids, and directional calls with commas; spaces after commas are allowed. Include a trailing comma after each callsign or grid, and end each prefix with a semicolon. A leading comma is not required, and a single directional call needs no comma.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
               </property>
              </widget>
             </item>

HighlightingRules.cpp
@@ -1,0 +1,20 @@
+#include "HighlightingRules.hpp"
+
+#include <QStringList>
+
+namespace HighlightingRules
+{
+  bool matchesDirectionalCall (QString const& configured_entries,
+                               QString const& directional_call)
+  {
+    auto const wanted = directional_call.trimmed ();
+    if (wanted.isEmpty ()) return false;
+
+    for (auto const& entry : configured_entries.split (','))
+      {
+        if (entry.trimmed ().compare (wanted, Qt::CaseInsensitive) == 0) return true;
+      }
+
+    return false;
+  }
+}

HighlightingRules.hpp
@@ -1,0 +1,12 @@
+#ifndef HIGHLIGHTING_RULES_HPP__
+#define HIGHLIGHTING_RULES_HPP__
+
+#include <QString>
+
+namespace HighlightingRules
+{
+  bool matchesDirectionalCall (QString const& configured_entries,
+                               QString const& directional_call);
+}
+
+#endif

widgets/mainwindow.cpp
@@ -115,6 +115,7 @@
 #include "PrefixSuffix.hpp"
 #include "CountryNames.hpp"
 #include "HelpText.hpp"
+#include "HighlightingRules.hpp"
 #include "Audio/WavFile.hpp"
 #include "WSJTXLogging.hpp"
 #include "Logger.hpp"
@@ -2606,11 +2607,13 @@ void MainWindow::fastSink(qint64 frames)
         }
         // highlight directional calls
         if (tw.size () > 2) {
-          if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ",")) {
+          if (m_config.highlight_orange() && tw[0]=="CQ"
+              && HighlightingRules::matchesDirectionalCall(m_config.highlight_orange_callsigns(), tw[1])) {
             ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
             if (m_config.alert_Enabled() && m_config.alert_Wanted() && !m_muted && tw[1]!="") play_Wanted = true;
           }
-          if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ",")) {
+          if (m_config.highlight_blue() && tw[0]=="CQ"
+              && HighlightingRules::matchesDirectionalCall(m_config.highlight_blue_callsigns(), tw[1])) {
             ui->decodedTextBrowser->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
             if (m_config.alert_Enabled() && m_config.alert_Wanted() && !m_muted && tw[1]!="") play_Wanted = true;
           }
@@ -14760,11 +14763,13 @@ void MainWindow::applyHighlighting(const DecodedText& decodedtext, DisplayText *
       tw=text.mid(22).split(" ",SkipEmptyParts);
     }
     if (tw.size() > 2) {
-      if (m_config.highlight_orange() && tw[0]=="CQ" && m_config.highlight_orange_callsigns().contains("," + tw[1] + ",")) {
+      if (m_config.highlight_orange() && tw[0]=="CQ"
+          && HighlightingRules::matchesDirectionalCall(m_config.highlight_orange_callsigns(), tw[1])) {
         decodePane->highlight_callsign(tw[1], QColor(225,75,0), QColor(255,255,255), true);
         if (updateAlertState && m_config.alert_Enabled() && m_config.alert_Wanted() && !m_muted && tw[1]!="") play_Wanted = true;
       }
-      if (m_config.highlight_blue() && tw[0]=="CQ" && m_config.highlight_blue_callsigns().contains("," + tw[1] + ",")) {
+      if (m_config.highlight_blue() && tw[0]=="CQ"
+          && HighlightingRules::matchesDirectionalCall(m_config.highlight_blue_callsigns(), tw[1])) {
         decodePane->highlight_callsign(tw[1], QColor(0,100,255), QColor(255,255,255), true);
         if (updateAlertState && m_config.alert_Enabled() && m_config.alert_Wanted() && !m_muted && tw[1]!="") play_Wanted = true;
       }

tests/unit/common/CMakeLists.txt
@@ -31,6 +31,13 @@ add_executable (test_fox_guard_bands
 target_link_libraries (test_fox_guard_bands Qt5::Test)
 add_test (NAME test_fox_guard_bands COMMAND test_fox_guard_bands)
 
+add_executable (test_highlighting_rules
+  test_highlighting_rules.cpp
+  ${CMAKE_SOURCE_DIR}/HighlightingRules.cpp
+)
+target_link_libraries (test_highlighting_rules Qt5::Test)
+add_test (NAME test_highlighting_rules COMMAND test_highlighting_rules)
+
 add_executable (test_tx_start_policy
   test_tx_start_policy.cpp
 )

tests/unit/common/test_highlighting_rules.cpp
@@ -1,0 +1,47 @@
+#include <QtTest>
+
+#include "HighlightingRules.hpp"
+
+class TestHighlightingRules
+  : public QObject
+{
+  Q_OBJECT
+
+private:
+  Q_SLOT void directional_call_matching_data ()
+  {
+    QTest::addColumn<QString> ("configured_entries");
+    QTest::addColumn<QString> ("directional_call");
+    QTest::addColumn<bool> ("matches");
+
+    QTest::newRow ("first entry") << "DX,POTA,SOTA," << "DX" << true;
+    QTest::newRow ("middle entry") << "DX,POTA,SOTA," << "POTA" << true;
+    QTest::newRow ("last entry") << "DX,POTA,SOTA," << "SOTA" << true;
+    QTest::newRow ("last without trailing comma") << "DX,POTA,SOTA" << "SOTA" << true;
+    QTest::newRow ("documented spaces") << "DX, POTA, SOTA," << "POTA" << true;
+    QTest::newRow ("single entry") << "POTA" << "POTA" << true;
+    QTest::newRow ("single trailing comma") << "POTA," << "POTA" << true;
+    QTest::newRow ("legacy leading comma") << ",POTA," << "POTA" << true;
+    QTest::newRow ("empty fields") << ",, POTA ,," << "POTA" << true;
+    QTest::newRow ("lowercase entry") << "dx,pota," << "POTA" << true;
+    QTest::newRow ("callsign suffix") << "K1DX," << "DX" << false;
+    QTest::newRow ("longer token") << "NOTPOTA," << "POTA" << false;
+    QTest::newRow ("prefix syntax") << "VK;ZL;T88;" << "VK" << false;
+    QTest::newRow ("missing entry") << "DX,POTA," << "SOTA" << false;
+    QTest::newRow ("empty configuration") << "" << "DX" << false;
+    QTest::newRow ("empty directional call") << "DX," << "" << false;
+  }
+
+  Q_SLOT void directional_call_matching ()
+  {
+    QFETCH (QString, configured_entries);
+    QFETCH (QString, directional_call);
+    QFETCH (bool, matches);
+
+    QCOMPARE (HighlightingRules::matchesDirectionalCall (configured_entries, directional_call), matches);
+  }
+};
+
+QTEST_MAIN (TestHighlightingRules);
+
+#include "test_highlighting_rules.moc"