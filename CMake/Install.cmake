adeac2c9b8c0b1147cc9075e9d9cff27a78b7a49
David Christle <dchristle@users.noreply.github.com>
2026-07-26 13:16:09 -0500

    docs(readme): refresh project overview

--- CMake/Install.cmake
+++ CMake/Install.cmake
@@ -100,7 +100,13 @@ install (PROGRAMS
   )
 
 install (FILES
-  README
+  README.md
+  DESTINATION ${CMAKE_INSTALL_DOCDIR}
+  RENAME README
+  #COMPONENT runtime
+  )
+
+install (FILES
   COPYING
   AUTHORS
   THANKS
